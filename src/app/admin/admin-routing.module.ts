ed9385bbc36790d31f5d04a7603966b1ac6874b1
Ivan Zhyhalkin <ivan.zhigalkin@gmail.com>
2022-07-11 21:33:20 +0300

    admin menu

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { AdminCartsComponent } from './admin-carts/admin-carts.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
 import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  {path:"admin",canActivate:[AuthService], component: AdminComponent },
+  {path:"admin",canActivate:[AuthService], component: AdminComponent, children:[
+    {path:"users",canActivate:[AuthService], component: AdminUsersComponent },
+    {path:"carts",canActivate:[AuthService], component: AdminCartsComponent},
+  ]},
 ];
 
 @NgModule({
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
+import { AdminUsersComponent } from './admin-users/admin-users.component';
+import { AdminCartsComponent } from './admin-carts/admin-carts.component';
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AdminUsersComponent,
+    AdminCartsComponent
   ],
   imports: [
     CommonModule,
